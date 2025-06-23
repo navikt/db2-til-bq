fb428a5f59d42eed04d3fc6f62af77d235b828b4
@x10an14-nav <christian.chavez@nav.no>
2025-06-23 17:53:22 +0200

    refactor/test: Verify license and add some debug prints before/after SQL execution

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,17 @@
 FROM python:3.12-slim
 
+# Update/install everything non-python
 RUN apt-get update && apt-get install -yq --no-install-recommends \
     build-essential \
     git
-
-COPY requirements.txt .
 RUN pip install --upgrade pip
+
+# Install python deps non-virtualenv-like
+COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
-RUN ln -s /var/run/secrets/db2-license/db2consv_zs.lic /usr/local/lib/python3.12/site-packages/clidriver/license/db2consv_zs.lic
 RUN pip install --upgrade --force-reinstall ibm_db
 
+RUN ln -s /var/run/secrets/db2-license/db2consv_zs.lic /usr/local/lib/python3.12/site-packages/clidriver/license/db2consv_zs.lic # Python 3.12 only
 COPY main.py .
+
 CMD [ "python", "./main.py" ]
--- main.py
+++ main.py
@@ -4,33 +4,33 @@ import pandas as pd
 
 from google.cloud import storage
 from google.oauth2 import service_account
-from google.cloud import bigquery
-import ibm_db
-import ibm_db_sa
+# from google.cloud import bigquery
+# import ibm_db
+# import ibm_db_sa
 from sqlalchemy import create_engine
 
 
-
 def read_from_db2(table_name):
-
     database_username = os.environ.get("DATABASE_USERNAME")
     database_password = os.environ.get("DATABASE_PASSWORD")
-    database_host = os.environ.get("DATABASE_HOST")
-    database_port = os.environ.get("DATABASE_PORT")
-    database_name = os.environ.get("DATABASE_NAME")
-    schema = os.environ.get("DATABASE_SCHEMA")
-
+    database_host = os.environ.get("DATABASE_HOST", default="155.55.1.82")
+    database_port = os.environ.get("DATABASE_PORT", default="5025")
+    database_name = os.environ.get("DATABASE_NAME", default="QDB2")
 
     # Establish the connection
     db2_connection_string = f"db2+ibm_db://{database_username}:{database_password}@{database_host}:{database_port}/{database_name}"
 
     engine = create_engine(db2_connection_string)
+    print(f"Engine created: {engine=}")
     connection = engine.connect() 
+    print(f"Connection established: {connection=}")
 
     #run query
+    schema = os.environ.get("DATABASE_SCHEMA", default="OS231Q1")
     sql = f"select * from {schema}.{table_name}"
 
-    df = pd.read_sql(sql, connection)
+    print("Starting sql read/extraction")
+    return pd.read_sql(sql, connection)
 
 def download_blob(bucket_name, source_blob_name, destination_file_name):
     storage_credentials = service_account.Credentials.from_service_account_file('/var/run/secrets/key')
@@ -41,10 +41,10 @@ def download_blob(bucket_name, source_blob_name, destination_file_name):
     blob.download_to_filename(destination_file_name)
 
 
-
 def main():
-    print("Starter jobb, leser lisens")
-    print(f"Lisens finnes: {os.path.isfile("/usr/local/lib/python3.12/site-packages/clidriver/license/db2consv_zs.lic")}")
+    print("Starter jobb")
+    filen_finnes = os.path.isfile("/usr/local/lib/python3.12/site-packages/clidriver/license/db2consv_zs.lic")
+    print(f"Ligger lisens der forventet?: {filen_finnes=}")
 
     #legge lisensen et sted
     #download_blob('lisens-db2_utsikt-dev-3609', 'db2consv_zs.lic', 'db2consv_zs.lic')
