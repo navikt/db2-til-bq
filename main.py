d8296a299af55a1ac1a68674ce3ad460806a1300
@x10an14-nav <christian.chavez@nav.no>
2025-06-26 10:36:59 +0200

    upkeep: Fjern død kode

--- main.py
+++ main.py
@@ -2,41 +2,7 @@
 
 import os
 import pandas as pd
-# from google.cloud import bigquery
 import ibm_db
-# import ibm_db_sa
-#from sqlalchemy import create_engine
-
-
-def read_from_db2(table_name):
-    database_username = os.environ.get("DATABASE_USERNAME")
-    database_password = os.environ.get("DATABASE_PASSWORD")
-    database_host = os.environ.get("DATABASE_HOST", default="155.55.1.82")
-    database_port = os.environ.get("DATABASE_PORT", default="5025")
-    database_name = os.environ.get("DATABASE_NAME", default="QDB2")
-
-# Establish the connection
-    db2_connection_string = f"db2+ibm_db://{database_username}:{database_password}@{database_host}:{database_port}/{database_name}"
-
-    engine = create_engine(db2_connection_string)
-    ibm_db.debug(True)
-    print(f"Engine created: {engine=}")
-    try:
-        connection = engine.connect()
-    except Exception as e:
-        print(f"Unable to instantiate DB2 connection:\n{e}")
-        import sys
-        sys.stderr.flush()
-        sys.stdout.flush()
-        return []
-    print(f"Connection established: {connection=}")
-
-    #run query
-    schema = os.environ.get("DATABASE_SCHEMA", default="OS231Q1")
-    sql = f"select * from {schema}.{table_name}"
-
-    print("Starting sql read/extraction")
-    return pd.read_sql(sql, connection)
 
 def read_from_db2_2(table_name):
     database_username = os.environ.get("DATABASE_USERNAME")
