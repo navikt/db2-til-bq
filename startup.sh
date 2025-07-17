103356d81795efef8d5d77949a3cf8186405cdf8
@x10an14-nav <christian.chavez@nav.no>
2025-07-17 14:19:57 +0200

    revert: Ensure to keep reference to actual file, and not (sym)link refs

--- startup.sh
+++ startup.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Copy the DB2 license file to the appropriate location
-cp /var/run/secrets/db2-license/db2consv_zs.lic /usr/local/lib/python3.12/site-packages/clidriver/license/db2consv_zs.lic
+cp "$(realpath /var/run/secrets/db2-license/db2consv_zs.lic)" /usr/local/lib/python3.12/site-packages/clidriver/license/
 
 # Run Python unbuffered to ensure logs are printed immediately using -u
 python -u ./main.py
